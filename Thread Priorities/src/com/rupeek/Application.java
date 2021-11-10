bb23bf82e47da9547361575a03b3a30836e2546e
Prasad-Kancharla <divya.kancharla.cn@rupeek.com>
2021-11-10 15:43:24 +0530

    Clean Up

--- Thread Priorities/src/com/rupeek/Application.java
+++ Thread Priorities/src/com/rupeek/Application.java
@@ -3,7 +3,7 @@ package com.rupeek;
 public class Application {
 
   public static void main(String[] args) {
-    System.out.println("Thread priority of Main thread is " + Thread.currentThread().getPriority());
+    System.out.println(" Priority of Main thread is " + Thread.currentThread().getPriority());
     MyThread t = new MyThread();
     t.start();
     System.out.println("Main thread continues");
--- Thread Priorities/src/com/rupeek/MyThread.java
+++ Thread Priorities/src/com/rupeek/MyThread.java
@@ -4,7 +4,7 @@ public class MyThread extends Thread{
 
   @Override
   public void run() {
-    //System.out.println("Thread priority of child thread is " + Thread.currentThread().getPriority());
+    //System.out.println("Priority of child thread is " + Thread.currentThread().getPriority());
     for(int i = 0; i<5; i++){
       System.out.println("Run method");
       Thread.yield();
