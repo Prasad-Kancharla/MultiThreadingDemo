3fad034ae8dd10f6b4f09a3c8d84fc5208b41514
Prasad-Kancharla <divya.kancharla.cn@rupeek.com>
2021-11-10 09:48:36 +0530

    Apply sleep method

---
+++ SleepMethod/src/com/rupeek/Application.java
@@ -0,0 +1,10 @@
+package com.rupeek;
+
+public class Application {
+
+  public static void main(String[] args) throws InterruptedException {
+    System.out.println("Demo of sleep method");
+    Thread.sleep(3000);
+    System.out.println("Waited 3 seconds");
+  }
+}
